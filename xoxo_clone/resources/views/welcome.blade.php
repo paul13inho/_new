8a6be559e3b6f082c96c6939953df3aa36661f71
Paulo <tamanini8@gmail.com>
2023-03-30 17:13:55 -0300

    project start

---
+++ xoxo_clone/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body class="flex justify-center h-screen w-screen place-items-center">
+<h1 class="text-3xl font-bold underline bg-teal-100 w-48 h-24 flex justify-center place-items-center">
+    Hello world!
+</h1>
+</body>
+</html>
